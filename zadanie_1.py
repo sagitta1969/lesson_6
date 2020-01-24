920ff57b5bef4bc31356dfc137245a5c247615bc
sagitta1969 <55909883+sagitta1969@users.noreply.github.com>
2020-01-24 12:18:13 +0300

    Update zadanie_1.py

--- zadanie_1.py
+++ zadanie_1.py
@@ -2,14 +2,7 @@ from time import sleep
 from itertools import cycle
 
 class TrafficLight:
-    __color = [['red',7], ['yellow', 2], ['green', 7], ['yellow', 7]]
-    
-    def running(self):
-        for el in cycle(self.__color):
-            for i, val in enumerate(self.__color):
-                print(self.__color[i][0])
-                sleep(self.__color[i][1])
-        
+    __color = [['red',7], ['yellow', 2], ['green', 7], ['yellow', 7]]     
         
         def running_1(self):
             while True:
@@ -21,11 +14,6 @@ class TrafficLight:
                 sleep(7)
                 print("yellow")
                 sleep(2)
-
-
-color_1 = TrafficLight()
-color_1.running() 
-
 
 
 trafficLight = TrafficLight()
